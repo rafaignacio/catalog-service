4e8f53b8f017d755fe948524a18a51a566664450
Rafael Ignacio Pinto <ignacio.rafael@gmail.com>
2023-04-20 19:19:51 +0200

    adding retry policy

--- src/CatalogService.Infrastructure/MessageBroker.cs
+++ src/CatalogService.Infrastructure/MessageBroker.cs
@@ -7,12 +7,16 @@ namespace CatalogService.Infrastructure;
 public class MessageBroker
 {
     private string _connectionString;
+    private const short MaxRetries = 5;
 
     public MessageBroker(string connectionString) =>
         _connectionString = connectionString;
 
-    public void SendMessage<T>(string queueName, string eventName, T @event)
+    public void SendMessage<T>(string queueName, string eventName, T @event, short retryNumber = 1)
     {
+        if (retryNumber > MaxRetries)
+            throw new TimeoutException($"Failed to deliver message after {MaxRetries} retries.");
+
         var connectionFactory = new ConnectionFactory()
         {
             HostName = _connectionString,
@@ -26,12 +30,21 @@ public class MessageBroker
             autoDelete: false);
         channel.QueueBind(queueName, eventName, "", null);
 
+        var properties = channel.CreateBasicProperties();
+        properties.Persistent = true;
+
         var jsonBody = JsonSerializer.Serialize(@event);
 
         channel.BasicPublish(exchange: eventName,
             mandatory: true,
-            basicProperties: null,
+            basicProperties: properties,
             routingKey: "",
             body: Encoding.UTF8.GetBytes(jsonBody));
+
+        channel.ConfirmSelect();
+        if(!channel.WaitForConfirms(TimeSpan.FromSeconds(5)))
+        {
+            SendMessage(queueName, eventName, @event, ++retryNumber);
+        }
     }
 }
